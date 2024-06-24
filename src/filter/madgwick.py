f1a27f07c633ba47779c2a9367cb92dc938aa5c2
DuelitDev <jyoon07dev@gmail.com>
2024-06-25 00:44:23 +0900

    Refactor equations for improved readability in Madgwick filter
    The original equations in “madgwick.py” have been simplified for better readability without compromising the functionality. The changes mostly concern the excessive parentheses, which have been reduced. The simplification contributes to easier maintenance and understanding of the code.

--- src/filter/madgwick.py
+++ src/filter/madgwick.py
@@ -22,55 +22,12 @@ class MadgwickFilter(Filter):
         q_dot = .5 * q * Quaternion(0, g.x, g.y, g.z)
         h = q * Quaternion(0, m.x, m.y, m.z) * ~q
         b = math.sqrt(h.x ** 2 + h.y ** 2)
-        s0 = (-(2 * q.y) * (2.0 * (q.x * q.z) - (2 * q.w * q.y) - a.x) + (
-                    2 * q.x) * (2.0 * (q.w * q.x) + (
-                    2 * q.y * q.z) - a.y) - h.z * q.y * (
-                          b * (0.5 - (q.y * q.y) - (q.z * q.z)) + h.z * (
-                              (q.x * q.z) - (q.w * q.y)) - m.x) + (
-                          -b * q.z + h.z * q.x) * (
-                          b * ((q.x * q.y) - (q.w * q.z)) + h.z * (
-                              (q.w * q.x) + (q.y * q.z)) - m.y) + b * q.y * (
-                          b * ((q.w * q.y) + (q.x * q.z)) + h.z * (
-                              0.5 - (q.x * q.x) - (q.y * q.y)) - m.z))
-        s1 = ((2 * q.z) * (2.0 * (q.x * q.z) - (2 * q.w * q.y) - a.x) + (
-                    2 * q.w) * (2.0 * (q.w * q.x) + (
-                    2 * q.y * q.z) - a.y) - 4.0 * q.x * (
-                          1 - 2.0 * (q.x * q.x) - 2.0 * (
-                              q.y * q.y) - a.z) + h.z * q.z * (
-                          b * (0.5 - (q.y * q.y) - (q.z * q.z)) + h.z * (
-                              (q.x * q.z) - (q.w * q.y)) - m.x) + (
-                          b * q.y + h.z * q.w) * (
-                          b * ((q.x * q.y) - (q.w * q.z)) + h.z * (
-                              (q.w * q.x) + (q.y * q.z)) - m.y) + (
-                          b * q.z - (2 * h.z) * q.x) * (
-                          b * ((q.w * q.y) + (q.x * q.z)) + h.z * (
-                              0.5 - (q.x * q.x) - (q.y * q.y)) - m.z))
-        s2 = (-(2 * q.w) * (2.0 * (q.x * q.z) - (2 * q.w * q.y) - a.x) + (
-                    2 * q.z) * (2.0 * (q.w * q.x) + (
-                    2 * q.y * q.z) - a.y) - 4.0 * q.y * (
-                          1 - 2.0 * (q.x * q.x) - 2.0 * (q.y * q.y) - a.z) + (
-                          -(2 * b) * q.y - h.z * q.w) * (
-                          b * (0.5 - (q.y * q.y) - (q.z * q.z)) + h.z * (
-                              (q.x * q.z) - (q.w * q.y)) - m.x) + (
-                          b * q.x + h.z * q.z) * (
-                          b * ((q.x * q.y) - (q.w * q.z)) + h.z * (
-                              (q.w * q.x) + (q.y * q.z)) - m.y) + (
-                          b * q.w - (2 * h.z) * q.y) * (
-                          b * ((q.w * q.y) + (q.x * q.z)) + h.z * (
-                              0.5 - (q.x * q.x) - (q.y * q.y)) - m.z))
-        s3 = ((2 * q.x) * (2.0 * (q.x * q.z) - (2 * q.w * q.y) - a.x) + (
-                    2 * q.y) * (2.0 * (q.w * q.x) + (2 * q.y * q.z) - a.y) + (
-                          -(2 * b) * q.z + h.z * q.x) * (
-                          b * (0.5 - (q.y * q.y) - (q.z * q.z)) + h.z * (
-                              (q.x * q.z) - (q.w * q.y)) - m.x) + (
-                          -b * q.w + h.z * q.y) * (
-                          b * ((q.x * q.y) - (q.w * q.z)) + h.z * (
-                              (q.w * q.x) + (q.y * q.z)) - m.y) + b * q.x * (
-                          b * ((q.w * q.y) + (q.x * q.z)) + h.z * (
-                              0.5 - (q.x * q.x) - (q.y * q.y)) - m.z))
+        s0 = (-(2 * q.y) * (2 * (q.x * q.z) - (2 * q.w * q.y) - a.x) + (2 * q.x) * (2 * (q.w * q.x) + (2 * q.y * q.z) - a.y) - h.z * q.y * (b * (.5 - (q.y * q.y) - (q.z * q.z)) + h.z * ((q.x * q.z) - (q.w * q.y)) - m.x) + (-b * q.z + h.z * q.x) * (b * ((q.x * q.y) - (q.w * q.z)) + h.z * ((q.w * q.x) + (q.y * q.z)) - m.y) + b * q.y * (b * ((q.w * q.y) + (q.x * q.z)) + h.z * (.5 - (q.x * q.x) - (q.y * q.y)) - m.z))
+        s1 = ((2 * q.z) * (2 * (q.x * q.z) - (2 * q.w * q.y) - a.x) + (2 * q.w) * (2 * (q.w * q.x) + (2 * q.y * q.z) - a.y) - 4 * q.x * (1 - 2 * (q.x * q.x) - 2 * (q.y * q.y) - a.z) + h.z * q.z * (b * (.5 - (q.y * q.y) - (q.z * q.z)) + h.z * ((q.x * q.z) - (q.w * q.y)) - m.x) + (b * q.y + h.z * q.w) * (b * ((q.x * q.y) - (q.w * q.z)) + h.z * ((q.w * q.x) + (q.y * q.z)) - m.y) + (b * q.z - (2 * h.z) * q.x) * (b * ((q.w * q.y) + (q.x * q.z)) + h.z * (.5 - (q.x * q.x) - (q.y * q.y)) - m.z))
+        s2 = (-(2 * q.w) * (2 * (q.x * q.z) - (2 * q.w * q.y) - a.x) + (2 * q.z) * (2 * (q.w * q.x) + (2 * q.y * q.z) - a.y) - 4 * q.y * (1 - 2 * (q.x * q.x) - 2 * (q.y * q.y) - a.z) + (-(2 * b) * q.y - h.z * q.w) * (b * (.5 - (q.y * q.y) - (q.z * q.z)) + h.z * ((q.x * q.z) - (q.w * q.y)) - m.x) + (b * q.x + h.z * q.z) * (b * ((q.x * q.y) - (q.w * q.z)) + h.z * ((q.w * q.x) + (q.y * q.z)) - m.y) + (b * q.w - (2 * h.z) * q.y) * (b * ((q.w * q.y) + (q.x * q.z)) + h.z * (.5 - (q.x * q.x) - (q.y * q.y)) - m.z))
+        s3 = ((2 * q.x) * (2 * (q.x * q.z) - (2 * q.w * q.y) - a.x) + (2 * q.y) * (2 * (q.w * q.x) + (2 * q.y * q.z) - a.y) + (-(2 * b) * q.z + h.z * q.x) * (b * (.5 - (q.y * q.y) - (q.z * q.z)) + h.z * ((q.x * q.z) - (q.w * q.y)) - m.x) + (-b * q.w + h.z * q.y) * (b * ((q.x * q.y) - (q.w * q.z)) + h.z * ((q.w * q.x) + (q.y * q.z)) - m.y) + b * q.x * (b * ((q.w * q.y) + (q.x * q.z)) + h.z * (.5 - (q.x * q.x) - (q.y * q.y)) - m.z))
         s = Quaternion(s0, s1, s2, s3)
         s.normalize()
-
         q_dot -= Quaternion(self._beta * s.w, self._beta * s.x,
                             self._beta * s.y, self._beta * s.z)
         q += Quaternion(q_dot.w * dt, q_dot.x * dt, q_dot.y * dt, q_dot.z * dt)
