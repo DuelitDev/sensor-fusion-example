eafd5865e24dd1f91c778eb409a8ba99c1f076e3
DuelitDev <100027958+DuelitDev@users.noreply.github.com>
2024-10-21 01:24:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ sensor.set_calibrate(
     gx=0.0, gy=0.0, gz=0.0,
     mx=0.0, my=0.0, mz=0.0
 )
+# sensor = SerialSensor(filter_=filter_, port="COM3")
+# sensor.set_order(a=Order.YXZ, g=Order.YXZ, m=Order.YXZ)
+# sensor.set_invert(ax=True, ay=True, gz=True)
+# sensor.set_calibrate(
+#     ax=0.0, ay=0.0, az=0.0,
+#     gx=0.0, gy=0.0, gz=0.0,
+#     mx=0.0, my=0.0, mz=0.0
+# )
 
 
 app = Ursina()
