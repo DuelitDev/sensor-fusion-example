0b053c5f984acf8e8302edb8b250693604ff2319
DuelitDev <100027958+DuelitDev@users.noreply.github.com>
2024-10-21 01:27:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,23 @@ from ursina import *
 filter_ = FusionFilter(FusionFilterOptions())
 sensor = UDPSensor(filter_=filter_, address="0.0.0.0", port=8888)
 sensor.set_order(a=Order.YXZ, g=Order.YXZ, m=Order.YXZ)
-sensor.set_invert(ax=True, ay=True, gz=True)
+sensor.set_invert(
+    ax=True,  ay=True,  az=False,
+    gx=False, gy=False, gz=True,
+    mx=False, my=False, mz=False
+)
 sensor.set_calibrate(
     ax=0.0, ay=0.0, az=0.0,
     gx=0.0, gy=0.0, gz=0.0,
     mx=0.0, my=0.0, mz=0.0
 )
 # sensor = SerialSensor(filter_=filter_, port="COM3")
-# sensor.set_order(a=Order.YXZ, g=Order.YXZ, m=Order.YXZ)
-# sensor.set_invert(ax=True, ay=True, gz=True)
+# sensor.set_order(a=Order.YXZ, g=Order.YXZ, m=Order.XYZ)
+# sensor.set_invert(
+#     ax=True,  ay=True,  az=False,
+#     gx=False, gy=False, gz=True,
+#     mx=False, my=False, mz=False
+# )
 # sensor.set_calibrate(
 #     ax=0.0, ay=0.0, az=0.0,
 #     gx=0.0, gy=0.0, gz=0.0,
