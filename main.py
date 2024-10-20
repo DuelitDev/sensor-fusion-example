428165b033d293903fda9bd5545a5da2f363f84b
DuelitDev <100027958+DuelitDev@users.noreply.github.com>
2024-10-21 01:22:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,22 +9,14 @@ sensor = UDPSensor(filter_=filter_, address="0.0.0.0", port=8888)
 sensor.set_order(a=Order.YXZ, g=Order.YXZ, m=Order.YXZ)
 sensor.set_invert(ax=True, ay=True, gz=True)
 sensor.set_calibrate(
-    ax=0.003237, ay=-0.011006, az=0.005022,
-    gx=-0.001501, gy=-0.005425, gz=0.001142,
-    mx=2.099998, my=4.124998, mz=0.299999
+    ax=0.0, ay=0.0, az=0.0,
+    gx=0.0, gy=0.0, gz=0.0,
+    mx=0.0, my=0.0, mz=0.0
 )
-# sensor = SerialSensor(filter_=filter_, port="/dev/cu.usbserial-0001")
-# sensor.set_order(a=Order.YXZ, g=Order.YXZ, m=Order.XYZ)
-# sensor.set_invert(ax=True, ay=True, gz=True)
-# sensor.set_calibrate(
-#     ax=-0.006249, ay=-0.019650, az=0.004745,
-#     gx=0.001802, gy=0.930908, gz=-0.924316,
-#     mx=-33.211478, my=30.362637, mz=0.000000
-# )
 
 
 app = Ursina()
-model = Entity(model="resources/plane.obj", scale=(-0.5, -0.5, 0.5))
+model = Entity(model="resources/plane.obj")
 
 
 def update():
